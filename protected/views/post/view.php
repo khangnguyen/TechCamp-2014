b661d173f9b1a08199eebd776ee5ed9fa2fa63de
Khang Nguyen <nguyenkhang90@gmail.com>
2013-11-24 15:16:38 +0700

    KNN: integrated Disqus, piece of cake

--- protected/views/post/view.php
+++ protected/views/post/view.php
@@ -32,3 +32,20 @@ $this->menu=array(
 		'updated_at',
 	),
 )); ?>
+
+
+    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+        var disqus_shortname = 'techcampsaigon'; // required: replace example with your forum shortname
+        #var disqus_url = 'http://localhost:9250/post/view/id/2/title/1+This+is+awesome+man';
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+    
